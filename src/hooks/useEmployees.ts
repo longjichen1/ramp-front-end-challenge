f54bfb438bf464bd744c1870e3342511a3ffafb4
Jerry Chen <jerrychen@jims-macbook-pro-2.local>
2023-08-23 19:47:32 -0500

    first commit

---
+++ src/hooks/useEmployees.ts
@@ -0,0 +1,21 @@
+import { useCallback, useState } from "react"
+import { Employee } from "../utils/types"
+import { useCustomFetch } from "./useCustomFetch"
+import { EmployeeResult } from "./types"
+import { EMPTY_EMPLOYEE } from "src/utils/constants"
+
+export function useEmployees(): EmployeeResult {
+  const { fetchWithCache, loading } = useCustomFetch()
+  const [employees, setEmployees] = useState<Employee[] | null>(null)
+
+  const fetchAll = useCallback(async () => {
+    const employeesData = await fetchWithCache<Employee[]>("employees")
+    setEmployees(employeesData)
+  }, [fetchWithCache])
+
+  const invalidateData = useCallback(() => {
+    setEmployees(null)
+  }, [])
+
+  return { data: employees, loading, fetchAll, invalidateData }
+}
